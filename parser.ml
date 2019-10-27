cd4fec4df2b2af7b2ccc50581242a96295f1d822
Sarah <hal340@ucsd.edu>
2019-10-27 16:00:31 -0700

    old parser

--- parser.ml
+++ parser.ml
@@ -12,11 +12,47 @@ let int_of_string_opt s =
   try Some(int_of_string s) with
   | _ -> None
 
-let rec parse (sexp : Sexp.t) =
-  (* TODO *) failwith "Not yet implemented"
-
-and parse_binding binding =
-  (* TODO *) failwith "Not yet implemented"
+let rec parse sexp = match sexp with
+  | Atom(s) -> (if (Str.string_match number_regex s 0)
+                then (match int_of_string_opt s with
+                  | Some(n) -> ENumber(n)
+                  | None -> failwith "Non-representable number")
+                else (if (Str.string_match valid_id_regex s 0) 
+                      then (if check_const s reserved_constants 
+                            then EBool((string_toBool s reserved_constants))
+                            else (if check_reserved s reserved_words then failwith "Invalid" else EId(s))) 
+                      else failwith "Invalid id"))
+  | sexps -> match sexps with
+    | List([Atom("add1"); arg]) -> EPrim1(Add1, parse arg)
+    | List([Atom("sub1"); arg]) -> EPrim1(Sub1, parse arg)
+    | List([Atom("isNum"); arg]) -> EPrim1(IsNum, parse arg)
+    | List([Atom("isBool"); arg]) -> EPrim1(IsBool, parse arg)
+    | List([Atom("+"); arg1; arg2]) -> EPrim2(Plus, parse arg1, parse arg2)
+    | List([Atom("-"); arg1; arg2]) -> EPrim2(Minus, parse arg1, parse arg2)
+    | List([Atom("*"); arg1; arg2]) -> EPrim2(Times, parse arg1, parse arg2)
+    | List([Atom("<"); arg1; arg2]) -> EPrim2(Less, parse arg1, parse arg2)
+    | List([Atom(">"); arg1; arg2]) -> EPrim2(Greater, parse arg1, parse arg2)
+    | List([Atom("=="); arg1; arg2]) -> EPrim2(Equal, parse arg1, parse arg2)
+    | List([Atom("if"); arg1; arg2; arg3]) -> EIf(parse arg1, parse arg2, parse arg3)
+    | List([Atom("set"); Atom(s); arg1]) -> ESet(s, parse arg1)
+    | List(a) -> (match a with
+      | Atom("let")::List(e1)::rest -> (let binds = List.map parse_binding e1 in 
+                                        let bod = List.map parse rest in 
+                                        ELet(binds, bod))
+      | Atom("while")::arg1::rest -> EWhile(parse arg1, List.map parse rest)
+      | _ -> failwith "Invalid" )
+and parse_binding binding = match binding with 
+  | List([Atom(name); arg]) -> if (check_reserved name reserved_words) then failwith "Invalid" else (name, parse arg)
+  | _ -> failwith "Invalid"
+and check_reserved name list = match list with 
+  | [] -> false
+  | head::tail -> if (name = head) then true else (check_reserved name tail)
+and check_const name list = match list with 
+  | [] -> false
+  | head::tail -> if (name = head) then true else (check_const name tail)
+and string_toBool name list = match list with
+  | head::tail -> if (head = name) then true else false
+  | _ -> failwith "boolean creation messed up"
 
 let parse_def sexp =
   (* TODO *) failwith "Not yet implemented"
